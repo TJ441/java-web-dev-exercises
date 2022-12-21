aef86be263f1c8a916567da8c5138da5ceb3759b
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-21 05:24:48 -0600

    wrote helloworld with name prompt

--- src/org/launchcode/java/demos/lsn1datatypes/TempConverter.java
+++ src/org/launchcode/java/demos/lsn1datatypes/TempConverter.java
@@ -3,17 +3,17 @@ package org.launchcode.java.demos.lsn1datatypes;
 import java.util.Scanner;
 
 public class TempConverter {
-    public static void main(String[] args) {
-        double fahrenheit;
-        double celsius;
-        Scanner input;
+  public static void main(String[] args) {
+    double fahrenheit;
+    double celsius;
+    Scanner input;
 
-        input = new Scanner(System.in);
-        System.out.println("Enter the temperature in Fahrenheit: ");
-        fahrenheit = input.nextDouble();
-        input.close();
+    input = new Scanner(System.in);
+    System.out.println("Enter the temperature in Fahrenheit: ");
+    fahrenheit = input.nextDouble();
+    input.close();
 
-        celsius = (fahrenheit - 32) * 5/9;
-        System.out.println("The temperature in Celsius is: " + celsius + "°C");
-    }
+    celsius = (fahrenheit - 32) * 5/9;
+    System.out.println("The temperature in Celsius is: " + celsius + "°C");
+  }
 }
