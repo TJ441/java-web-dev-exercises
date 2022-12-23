b1e12541fb5f6b094e81df6e36b3259a5641046f
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-23 06:07:13 -0600

    Completed Character Count

--- src/Studio1/CountCharacters.java
+++ src/Studio1/CountCharacters.java
@@ -1,26 +1,21 @@
 package Studio1;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 public class CountCharacters {
   public static void main(String[] args) {
-    String paragraph = "If the product of two terms is zero then common sense says at least " +
-                       "one of the two terms has to be zero to start with. So if you move all the terms " +
-                       "over to one side, you can put the quadratics into a form that can be factored allowing " +
-                       "that side of the equation to equal zero. Once you’ve done that, it’s pretty " +
-                       "straightforward from there.";
+    String paragraph = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
     char[] charArray = paragraph.toCharArray();
+    HashMap<Character, Integer> charCounts = new HashMap<>();
 
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter character to count: ");
-    String charToConvert = input.next();
-    char[] converted = charToConvert.toCharArray();
-    char character = converted[0];
-
-    int count = 0;
-    for (char c : charArray)
-      if (c == character)
-        count++;
-
-    System.out.print("Number of " + character + "'s = " + count);
-    input.close();
+    for (char character : charArray) {
+      int count = 0;
+      for (char c : charArray)
+        if (c == character)
+          count++;
+      charCounts.put(character, count);
+    }
+    for (Map.Entry<Character, Integer> character : charCounts.entrySet()) {
+      System.out.println(character.getKey() + ": " + character.getValue());
+    }
   }
 }
