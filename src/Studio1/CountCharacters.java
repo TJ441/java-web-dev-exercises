f8331aa03dcd46d25918f68995dac6346bca0955
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-23 06:14:24 -0600

    Completed Character Count plus two out of three bonuses

--- src/Studio1/CountCharacters.java
+++ src/Studio1/CountCharacters.java
@@ -1,10 +1,13 @@
 package Studio1;
+import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 public class CountCharacters {
   public static void main(String[] args) {
-    String paragraph = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-    char[] charArray = paragraph.toCharArray();
+    Scanner input = new Scanner(System.in);
+    System.out.print("Type in text to analyze: ");
+    String paragraph = input.nextLine();
+    char[] charArray = paragraph.toLowerCase().toCharArray();
     HashMap<Character, Integer> charCounts = new HashMap<>();
 
     for (char character : charArray) {
