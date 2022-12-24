105c7750d22b4c981d7345a70478e874b04e300d
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-24 12:10:01 -0600

    Modified SchoolPractice and Students

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -2,6 +2,8 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
   public static void main(String[] args) {
-    Student newStudent = new Student("Tim Johnson", 547, 1, 4.0);
+    Student firstStudent = new Student("Tim Johnson", 30, 3.0);
+    Student secondStudent = new Student("Stacey Bullock", 30);
+
   }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -1,21 +1,30 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
+  private static int nextStudentId = 100;
   private String name;
-  private int studentId;
-  private int numberOfCredits = 0;
-  private double gpa = 0.0;
+  private final int studentId;
+  private int numberOfCredits;
+  private double gpa;
 
-  public Student(String name, int studentId, int numberOfCredits, double gpa) {
+  public Student(String name, int numberOfCredits, double gpa) {
     this.name = name;
-    this.studentId = studentId;
+    this.studentId = nextStudentId;
     this.numberOfCredits = numberOfCredits;
     this.gpa = gpa;
+    nextStudentId++;
+  }
+
+  public Student(String name, int numberOfCredits) {
+    this(name, numberOfCredits, 0);
+  }
+
+  public Student(String name) {
+    this(name, 0, 0);
   }
 
   public void setName(String aName) { name = aName; }
-  public void setStudentId(int aStudentId) { studentId = aStudentId; }
-  private void setNumberOfCredits(int aNumberOfCredits) { numberOfCredits = aNumberOfCredits; }
+  public void setNumberOfCredits(int aNumberOfCredits) { numberOfCredits = aNumberOfCredits; }
   public void setGpa(double aGpa) { gpa = aGpa; }
 
   public String getName() { return name; }
