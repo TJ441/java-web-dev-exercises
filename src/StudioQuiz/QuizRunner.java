1010067189961639b93540acf2e0cd1af73daf5d
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-27 12:12:53 -0600

    Started Studio for Quiz

--- src/StudioQuiz/QuizRunner.java
+++ src/StudioQuiz/QuizRunner.java
@@ -3,6 +3,6 @@ package StudioQuiz;
 public class QuizRunner {
   public static void main(String[] args) {
     Quiz.printQuiz();
-
+    //build questions in here, constructor and functions in Quiz. Quiz is like Menu in the restaurant program
   }
 }
