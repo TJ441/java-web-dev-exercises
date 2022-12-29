3c85780e027afc02e4f2a4a205f2a7f3b68d08c0
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-29 08:41:40 -0600

    cats

--- src/org/launchcode/java/demos/lsn6inheritance/Main.java
+++
@@ -1,8 +0,0 @@
-package org.launchcode.java.demos.lsn6inheritance;
-
-public class Main {
-  public static void main(String[] args) {
-    HouseCat garfield = new HouseCat("Garfield", 12, "orange");
-    System.out.print(garfield.getWeight());
-  }
-}
