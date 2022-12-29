fe27df4d6eb570ed5d9f91c4f10a52bf36ad76b4
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-29 08:41:15 -0600

    Flavor Comparator

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -3,17 +3,17 @@ package org.launchcode.java.demos.lsn7interfaces;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
-        Case menu = new Case();
-        ArrayList<Flavor> flavors = menu.getFlavors();
-        ArrayList<Cone> cones = menu.getCones();
+  public static void main(String[] args){
+    Case menu = new Case();
+    ArrayList<Flavor> flavors = menu.getFlavors();
+    ArrayList<Cone> cones = menu.getCones();
+    //Comparator comparator = new FlavorComparator();
+    // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
+    //  field.
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+    // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+    //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
-
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
-    }
+    // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+  }
 }
