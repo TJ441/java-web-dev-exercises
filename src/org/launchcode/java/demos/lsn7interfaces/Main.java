c2e32154dfc1ea0b68baf05376292692125cce3e
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-31 06:24:13 -0600

    Added cost sort

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,5 @@
 package org.launchcode.java.demos.lsn7interfaces;
 import java.util.ArrayList;
-import java.util.Comparator;
 
 public class Main {
   public static void main(String[] args){
@@ -8,11 +7,7 @@ public class Main {
     ArrayList<Flavor> flavors = menu.getFlavors();
     ArrayList<Cone> cones = menu.getCones();
     flavors.sort(new FlavorComparator());
-    System.out.print(flavors);
-
-
-    // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-    //  field.
+    cones.sort(new ConeComparator());
 
     // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
   }
